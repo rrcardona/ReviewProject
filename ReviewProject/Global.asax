647ae9f2d960ca4802311c30bbd0eecb37c1bad8
rrcardona <cardonas05@yahoo.com>
2017-10-19 20:43:29 -0400

    first commit

---
+++ ReviewProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReviewProject.MvcApplication" Language="C#" %>
---
+++ ReviewProject/Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ReviewProject.Models
+{
+    public class Review
+    {
+        [Key]
+        public string Name { get; set; }
+        public DateTime DateOfReview { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+
+
+
+
+    }
+}
